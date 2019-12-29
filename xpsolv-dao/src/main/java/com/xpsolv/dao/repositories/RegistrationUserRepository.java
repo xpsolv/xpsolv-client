14ba9911559b0bcfe0c07c7390590e60de818544
xpsolv <xpsolv@gmail.com>
2019-12-29 16:47:32 +0530

    class path changes

--- xpsolv-dao/src/main/java/com/xpsolv/dao/repositories/RegistrationUserRepository.java
+++ xpsolv-dao/src/main/java/com/xpsolv/dao/repositories/RegistrationUserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.xpsolv.dao.entity.RegistrationActorEntity;
-import com.xpsolv.model.registration.ActorRpdModel;
 
 @Repository
 public interface RegistrationUserRepository extends JpaRepository<RegistrationActorEntity, Integer> {
